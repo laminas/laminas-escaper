b906aa4c53506951358d603febca59c79e73865d
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:43:14 -0600

    Rewriting as Laminas Project package

--- test/EscaperTest.php
+++ test/EscaperTest.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-escaper for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-escaper/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-escaper/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Escaper;
+namespace LaminasTest\Escaper;
 
-use Zend\Escaper\Escaper;
+use Laminas\Escaper\Escaper;
 
 class EscaperTest extends \PHPUnit_Framework_TestCase
 {
@@ -171,7 +170,7 @@ class EscaperTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @expectedException \Zend\Escaper\Exception\InvalidArgumentException
+     * @expectedException \Laminas\Escaper\Exception\InvalidArgumentException
      */
     public function testSettingEncodingToEmptyStringShouldThrowException()
     {
@@ -186,7 +185,7 @@ class EscaperTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @expectedException \Zend\Escaper\Exception\InvalidArgumentException
+     * @expectedException \Laminas\Escaper\Exception\InvalidArgumentException
      */
     public function testSettingEncodingToInvalidValueShouldThrowException()
     {
